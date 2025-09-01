645e5ae1abef6046f66f9f81fee88b25373bec6a
Noah <Noah.junge@gmail.com>
2025-09-01 16:18:44 +0200

    Update line.py

--- line.py
+++ line.py
@@ -4,7 +4,7 @@ import robot
 arlo = robot.Robot()
 
 # Drive straight forward
-print(arlo.go_diff(64, 67, 1, 1))   # left=64, right=64, both forward
+print(arlo.go_diff(64, 66, 1, 1))   # left=64, right=64, both forward
 time.sleep(3)                       # move for 3 seconds
 
 # Stop
