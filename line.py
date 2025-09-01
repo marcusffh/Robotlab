48324255704fe56e929a1ae61ebd21ac4e4ec391
Noah <Noah.junge@gmail.com>
2025-09-01 15:54:07 +0200

    line

---
+++ line.py
@@ -0,0 +1,11 @@
+import time
+import robot
+
+arlo = robot.Robot()
+
+# Drive straight forward
+print(arlo.go_diff(64, 64, 1, 1))   # left=64, right=64, both forward
+time.sleep(3)                       # move for 3 seconds
+
+# Stop
+print(arlo.stop())
