eaccd9caff683210b980b8bec7e626c8367bc35b
Noah <Noah.junge@gmail.com>
2025-09-01 16:20:41 +0200

    Update line.py

--- line.py
+++ line.py
@@ -2,9 +2,10 @@ import time
 import robot
 
 arlo = robot.Robot()
-
+Straight = 64
+RightWheelError = 2
 # Drive straight forward
-print(arlo.go_diff(64, 66, 1, 1))   # left=64, right=64, both forward
+print(arlo.go_diff(Straight, Straight + 2, 1, 1))   # left=64, right=64, both forward
 time.sleep(3)                       # move for 3 seconds
 
 # Stop
