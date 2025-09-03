8ba40e76fd886a447104bb2736afc3be54e2a386
Noah <Noah.junge@gmail.com>
2025-09-03 12:28:59 +0200

    s

--- continousMovement.py
+++ continousMovement.py
@@ -21,6 +21,6 @@ def continous_drive(duration, leftSpeed, rightSpeed, leftDir, rightDir):
 
 for _ in range(1):  # figure-8 loops
     # curve left
-    continous_drive(4, speed - 35, speed + 35, FORWARD, FORWARD)
+    continous_drive(5, speed - 30, speed + 30, FORWARD, FORWARD)
     # curve right
-    continous_drive(4, speed + 35, speed - 35, FORWARD, FORWARD)
+    continous_drive(5, speed + 30, speed - 30, FORWARD, FORWARD)
