647ff9e3d407af2c985ad1dd9adbcd8c9c1c0137
Noah <Noah.junge@gmail.com>
2025-09-03 13:06:53 +0200

    s

--- square.py
+++ square.py
@@ -36,7 +36,7 @@ def drive_in_square(times=1):
             drive(speed, TURN_TIME, BACKWARD, FORWARD)    
             
 try:
-    drive_in_square(1) 
+    drive_in_square(3) 
 finally:
     arlo.stop() 
             
