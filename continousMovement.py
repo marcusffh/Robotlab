af301be47511f96a61a4eb03af9b34e4739dc8d2
Noah <Noah.junge@gmail.com>
2025-09-03 12:26:49 +0200

    s

--- continousMovement.py
+++ continousMovement.py
@@ -19,8 +19,8 @@ def continous_drive(duration, leftSpeed, rightSpeed, leftDir, rightDir):
     arlo.stop()
     time.sleep(0.2)  
 
-for _ in range(3):  # 3 figure-8 loops
+for _ in range(1):  # figure-8 loops
     # curve left
-    continous_drive(4, speed - 20, speed + 20, FORWARD, FORWARD)
+    continous_drive(4, speed - 30, speed + 30, FORWARD, FORWARD)
     # curve right
-    continous_drive(4, speed + 20, speed - 20, FORWARD, FORWARD)
+    continous_drive(4, speed + 30, speed - 30, FORWARD, FORWARD)
