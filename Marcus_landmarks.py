2a394ab92e6087dab3bd1651f91a4dfb80fbea34
monniiaa <sofielarsen2411@gmail.com>
2025-09-22 15:16:26 +0200

    s

--- Marcus_landmarks.py
+++ Marcus_landmarks.py
@@ -41,7 +41,7 @@ def drive_to_landmark():
                     last_id = marker_id
                     isDriving = False
         else:
-            calArlo.turn_angle(20)
+            calArlo.turn_angle(15)
 
     calArlo.stop()
 
