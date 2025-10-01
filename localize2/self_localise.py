0adcd3e36e735b14e3e485e9a00700af927ae22a
simonesskovgaard <160855277+simonesskovgaard@users.noreply.github.com>
2025-10-01 13:27:12 +0200

    Update self_localise.py

--- localize2/self_localise.py
+++ localize2/self_localise.py
@@ -47,10 +47,10 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 2]
+landmarkIDs = [9, 11]
 landmarks = {
-    1: (0.0, 0.0),  # Coordinates for landmark 1
-    2: (300.0, 0.0)  # Coordinates for landmark 2
+    9: (0.0, 0.0),  # Coordinates for landmark 1
+    11: (300.0, 0.0)  # Coordinates for landmark 2
 }
 landmark_colors = [CRED, CGREEN] # Colors used when drawing the landmarks
 
